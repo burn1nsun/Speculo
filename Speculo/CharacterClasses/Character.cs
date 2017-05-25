f4cc0400009d870a0dc43c83ec64dbac99c91026
burn1nsun <burn1nsunn@gmail.com>
2017-05-25 10:38:05 +0300

    Add project files.

---
+++ Speculo/CharacterClasses/Character.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Speculo.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speculo.CharacterClasses
+{
+    class Character
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        //private Vector2 velocity;
+        //private Vector2 origin;
+
+        private Rectangle rectangle;
+
+        SharedVariables sharedVariables = SharedVariables.Instance;
+
+        public Rectangle Rectangle
+        {
+            get { return this.rectangle; }
+            set { this.rectangle = value; }
+        }
+
+
+        public Character()
+        {
+            if (texture == null)
+            {
+                texture = sharedVariables.Content.Load<Texture2D>("Textures/character");
+            }
+
+            position = new Vector2(0, 0);
+            rectangle = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+            position.Y = 420;
+
+        }
+        public void Update(GameTime gameTime)
+        {
+            
+            MouseState newState = Mouse.GetState();
+            position.X = newState.X;
+            //position.Y = newState.Y;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, rectangle, Color.White);
+        }
+    }
+}
---
+++ Speculo/Utility/SharedVariables.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speculo.Utility
+{
+    public sealed class SharedVariables
+    {
+        private static SharedVariables instance = null;
+        private static readonly object padlock = new object();
+
+        public ContentManager Content { get; set; }
+        public GraphicsDevice Graphics { get; set; }
+
+        static bool called = false;
+        SharedVariables()
+        {
+
+
+        }
+
+        public static SharedVariables Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SharedVariables();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        internal void initVariables()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+
+}
